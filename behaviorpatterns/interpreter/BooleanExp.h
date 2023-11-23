5a986a2ca9e09e908fbbd6e08fbd73b7f836a304
bd878 <slavad878@gmail.com>
2023-11-23 21:05:16 +0300

    interpretator pattern

---
+++ behaviorpatterns/interpreter/BooleanExp.h
@@ -0,0 +1,8 @@
+class BooleanExp {
+public:
+  BooleanExp();
+  virtual ~BooleanExp();
+  virtual bool Evaluate(Context&) = 0;
+  virtual BooleanExp* Replace(const char*, BooleanExp&) = 0;
+  virtual BooleanExp* Copy() const = 0;
+};
---
+++ behaviorpatterns/interpreter/compute.cpp
@@ -0,0 +1,23 @@
+void compute() {
+  BooleanExp* expression;
+  Context context;
+
+  VariableExp* x = new VariableExp("X");
+  VariableExp* y = new VariableExp("Y");
+
+  expression = new OrExp(
+    new AndExp(new Constant(true), x),
+    new AndExp(y, new NotExp(x))
+  );
+
+  context.Assign(x, false);
+  context.Assign(y, true);
+
+  bool result = expression->Evaluate(context);
+
+  VariableExp* z = new VariableExp("Z");
+  NotExp not_z(z);
+  BooleanExp* replacement = expression->Replace("Y", not_z);
+  context.Assign(z, true);
+  result = replacement->Evaluate(context);
+}

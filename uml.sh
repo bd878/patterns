e4ee8772b084d4cdb6314c0a66ede7b78e3a3fa9
bd878 <slavad878@gmail.com>
2023-12-02 15:57:54 +0300

    compile uml diagrams next to patterns

---
+++ uml.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Compiles given uml file
+# to png image via plantuml script
+
+puml="/usr/local/lib/plantuml.jar"
+imgdir="images"
+[ $# -lt 1 ] && { echo "Usage: $0 umlfile"; exit 2; }
+umlf="$1"
+[ $? -ne 0 ] && { echo "Unknown file $1. Exit."; exit 2; }
+
+# dir/file.uml -> dir/$imgdir/file.png
+pngf=$(echo "`dirname $umlf`/$imgdir/`basename $umlf`" | sed s/\.uml/\.png/g);
+echo "save in $pngf"
+
+java -jar "$puml" "$umlf" -o "$imgdir"
+[ $? -eq 0 ] && xviewer "$pngf"
+
+exit 0;
+
